d69f4002748042a2d334fec539727949a7f1a5ad
Nathan <nathan.lam@zoho.com>
2020-09-13 22:05:56 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Tech Literacy Series Project: Backend
+
+This project is based on [this tutorial by Callicoder](https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/).
+
+## Prerequisites
+
+- MongoDB
+- NodeJS
+- yarn or npm
+
+## Test Command
+
+```
+curl -d '{"title":"Introduction","content":"Hello World!"}' -H 'Content-Type: application/json' http://localhost:3000/notes
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+app.get('/', (req, res) => {
+    res.json({
+        "message": "Hello World!"
+    })
+});
+
+require('./notes/notes.routes.js')(app);
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
